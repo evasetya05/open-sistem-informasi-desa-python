8d22e8b0a59be65d33d945df537e03215756d53f
evasetya05 <eva.setyabudi1986@gmail.com>
2025-10-17 16:05:01 +0700

    Normalize production environ.py

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,11 +1,10 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Coba deteksi apakah dijalankan di server LiteSpeed
+# Deteksi apakah server sedang dijalankan via LiteSpeed
 server_software = os.getenv('SERVER_SOFTWARE', '').lower()
-is_litespeed = 'litespeed' in server_software or 'lsws' in server_software
 
-if is_litespeed:
+if 'litespeed' in server_software or 'lsws' in server_software:
     settings_module = 'project.settings.production'
 else:
     settings_module = 'project.settings.development'
